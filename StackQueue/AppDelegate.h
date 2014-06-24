fb5836b1b5d861312d067991b453de53a04d2fdb
Bharath G M <gmbharath12@gmail.com>
2014-06-23 19:56:34 -0500

    Initial Commit

---
+++ StackQueue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StackQueue
+//
+//  Created by Bharath G M on 6/23/14.
+//  Copyright (c) 2014 Bharath G M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StackQueue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StackQueue
+//
+//  Created by Bharath G M on 6/23/14.
+//  Copyright (c) 2014 Bharath G M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
