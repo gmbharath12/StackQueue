a5d1be12d184c1a0c034b543eacda83ad88ed5d2
Bharath G M <gmbharath12@gmail.com>
2014-06-25 20:15:34 -0500

    Implementation of Queue..

--- StackQueue/Queue.h
+++ StackQueue/Queue.h
@@ -14,7 +14,14 @@
 }
 // Removes and returns the element at the front of the queue
 -(id)dequeue;
+
 // Add the element to the back of the queue
 -(void)enqueue:(id)element;
+
+// Returns YES if the queue is empty
+-(BOOL)isEmpty;
+
+// Returns the size of the queue
+-(NSInteger)size;
 
 @end
--- StackQueue/Queue.m
+++ StackQueue/Queue.m
@@ -10,5 +10,44 @@
 
 @implementation Queue
 
+-(id)init
+{
+    if ( (self = [super init]) )
+    {
+        m_cArray = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+-(id)dequeue
+{
+    if ([m_cArray count] > 0)
+    {
+		id object =  [m_cArray objectAtIndex:0] ;
+        [m_cArray removeObjectAtIndex:0];
+		return object;
+	}
+    else
+    {
+        return nil;
+    }
+}
+
+-(void)enqueue:(id)element
+{
+    [m_cArray addObject:element];
+}
+
+
+-(NSInteger)size
+{
+    return [m_cArray count];
+}
+
+-(BOOL)isEmpty
+{
+    return [m_cArray count] == 0;
+}
 
 @end
